ccbeaae4d7116df57072a1672156ae0a9d675ee2
Sibald Hulselmans <sibald.hulselmans@gmail.com>
2023-04-22 21:41:29 +0200

    Refactor: remove unneded file

--- src/services/api-client.ts
+++ src/services/api-client.ts
@@ -1,8 +1,13 @@
 import axios from "axios";
 
+export interface FetchResponse<T> {
+	count: number;
+	results: T[];
+}
+
 export default axios.create({
-    baseURL: 'https://api.rawg.io/api',
-    params: {
-        key: '8e47c3a25cf141da973b54d13867f024'
-    }
-})
+	baseURL: "https://api.rawg.io/api",
+	params: {
+		key: "8e47c3a25cf141da973b54d13867f024",
+	},
+});
